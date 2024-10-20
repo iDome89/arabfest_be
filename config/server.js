05cea03bb36e42be5e3a34e9b19c06168791ede7
ZecD <dominik.zecoli@gmail.com>
2024-10-20 15:53:51 +0200

    retrying config

--- config/server.js
+++ config/server.js
@@ -1,24 +1,13 @@
-module.exports = ({ env }) => {
-  const host = env('HOST', '0.0.0.0');
-  const port = env.int('PORT', 1337);
-  const appKeys = env.array('APP_KEYS');
-  const populateRelations = env.bool('WEBHOOKS_POPULATE_RELATIONS', false);
-  const externalUrl = env('RENDER_EXTERNAL_URL', 'https://arabfest-be.onrender.com'); // Add the external URL here
-
-  return {
-    host,
-    port,
-    url: externalUrl, // This is important for production deployments
-    app: {
-      keys: appKeys,
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: 'https://arabfest-be.onrender.com',  // Use your Render deployment URL here
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
     },
-    webhooks: {
-      populateRelations,
-    },
-    admin: {
-      auth: {
-        secret: env('ADMIN_JWT_SECRET'),
-      },
-    },
-  };
-};
+  },
+});
