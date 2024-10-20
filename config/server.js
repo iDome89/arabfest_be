f77de95434d9c1c83169d2ee29b05973f7f700ae
ZecD <dominik.zecoli@gmail.com>
2024-10-20 15:07:06 +0200

    fix

--- config/server.js
+++ config/server.js
@@ -1,10 +1,24 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  const host = env('HOST', '0.0.0.0');
+  const port = env.int('PORT', 1337);
+  const appKeys = env.array('APP_KEYS');
+  const populateRelations = env.bool('WEBHOOKS_POPULATE_RELATIONS', false);
+  const externalUrl = env('RENDER_EXTERNAL_URL', 'https://arabfest-be.onrender.com'); // Add the external URL here
+
+  return {
+    host,
+    port,
+    url: externalUrl, // This is important for production deployments
+    app: {
+      keys: appKeys,
+    },
+    webhooks: {
+      populateRelations,
+    },
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
+    },
+  };
+};
