b88e7dea52ef30110705c030d70e16e5ef64cf9a
ZecD <dominik.zecoli@gmail.com>
2024-10-20 15:45:45 +0200

    readded strapi cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:", env("RENDER_EXTERNAL_URL")], // Added RENDER_EXTERNAL_URL
+          "connect-src": ["'self'", "https:", env("RENDER_EXTERNAL_URL")], // Ensure Render URL is added here
           "default-src": ["'self'"],
           "img-src": [
             "'self'",
@@ -24,6 +24,16 @@ module.exports = ({ env }) => [
           ],
         },
       },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      credentials: true,
     },
   },
   "strapi::poweredBy",
